2d4d2b3851af675891ac277ecf2f8b1d083c9608
TB543666 <tb543666@gmail.com>
2018-02-08 14:49:31 -0600

    Phone

---
+++ phone/sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+}
+
+function keyPressed() {
+  noStroke();
+  fill(random(255), random(255), random(255), random(255));
+  ellipse(random(0, width), random(0, height), random(50, 200), random(50, 200));
+}
+
+function mouseClicked() {
+  noStroke();
+  fill(random(255), random(255), random(255), random(255));
+  rect(mouseX, mouseY, random(50, 200), random(50, 200));
+}
+
+function deviceShaken() {
+  fill(0);
+  textFont("Cambria");
+  textAlign(CENTER);
+  textSize(36);
+  text("My Name Jeff.", random(width), random(height));
+}
