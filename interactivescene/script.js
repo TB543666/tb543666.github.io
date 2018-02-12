5c8ad0fd00ec17c3b8c2c6316af3c74ed7693f5d
TB543666 <tb543666@gmail.com>
2018-02-12 15:20:19 -0600

    changes

--- interactivescene/script.js
+++ interactivescene/script.js
@@ -3,12 +3,14 @@ let baseValue = 20;
 function setup() {
 createCanvas(windowWidth,windowHeight);
 
+let button = createButton('Clear');
+button.position(30, 30);
 }
 
 function mouseDragged() {
   noStroke();
   //fill(random(255), random(255), random(255), random(255));
-  fill(0)
+  fill(random(255),random(255),random(255))
   ellipse(mouseX,mouseY,baseValue,baseValue);
 
 }
