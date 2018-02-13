e93eebbb0ee5d1e9b19ab5ec90fb70a26689a1a3
TB543666 <tb543666@gmail.com>
2018-02-13 15:20:02 -0600

    work period

--- interactivescene/script.js
+++ interactivescene/script.js
@@ -3,14 +3,21 @@ let baseValue = 20;
 function setup() {
 createCanvas(windowWidth,windowHeight);
 
-let button = createButton('Clear');
-button.position(30, 30);
+//let button = createButton('Clear');
+//button.position(30, 30);
+}
+
+function draw(){
+  rect(50,50,50,50);
 }
 
 function mouseDragged() {
   noStroke();
-  //fill(random(255), random(255), random(255), random(255));
-  fill(random(255),random(255),random(255))
+  fill(random(255), random(255), random(255), random(255));
   ellipse(mouseX,mouseY,baseValue,baseValue);
-
 }
+
+/*function mouseClicked(){
+  if (50 <= mouseX <= 100 && 50 <= mouseY <= 100){
+    background(0);
+*/
