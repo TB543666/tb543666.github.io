54a71215b3e677eae91fa6dcf429cf6da44f9bb4
TB543666 <tb543666@gmail.com>
2018-04-11 14:25:18 -0600

    update

--- life/sketch.js
+++ life/sketch.js
@@ -2,13 +2,21 @@
 // Dan Schellenberg
 // Mar 26, 2018
 
-let rows = 5;
-let cols = 5;
+let rows = 30;
+let cols = 30;
 let grid;
 let cellSize;
 
 function setup() {
   createCanvas(600, 600);
+
+  if(windowWidth > windowHeight){
+    createCanvas(windowHeight,windowHeight);
+  }
+  else{
+    createCanvas(windowWidth,windowWidth);
+  }
+
   cellSize = width / cols;
   grid = createRandom2dArray(cols, rows);
 }
@@ -16,6 +24,32 @@ function setup() {
 function draw() {
   background(255);
   displayGrid();
+}
+
+function nextTurn(){
+  for (let x=0; x<cols; x++) {
+    for (let y=0; y<rows; y++) {
+
+      let neighbors = 0;
+
+      for (let i=-1; i<1; i++){
+        for(let j=-1; i<1; j++){
+
+          if (x+i >= 0 && x+i < cols && y+j >= 0 && y+j < rows) {
+            neighbors += grid[x+i][y+j];
+          }
+        }
+      }
+      neighbors -= grid[x][y];
+
+      if (grid[x][y] === 1){
+        if (neighbors === 2 || neighbors === 3){
+          next[x][y] =1;         
+        }
+      }
+
+    }
+  }
 }
 
 function displayGrid() {
@@ -59,6 +93,17 @@ function createRandom2dArray(cols, rows) {
       else {
         randomGrid[x].push(1);
       }
+    }
+  }
+  return randomGrid;
+}
+
+function createEmpty2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x=0; x<cols; x++) {
+    randomGrid.push([]);
+    for (let y=0; y<rows; y++) {
+      randomGrid[x].push(0);
     }
   }
   return randomGrid;
